3e1bc4327ac274954f6bdb9ed2e690c127e98b4b
unknown <ejafari06@gmail.com>
2024-07-02 21:46:16 +0330

    potato

--- app/src/main/java/com/jafari/kotletapp/DatabaseHelper.java
+++ app/src/main/java/com/jafari/kotletapp/DatabaseHelper.java
@@ -10,14 +10,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private Context context;
 
-    public static final String DATABASE_NAME = "kotlet";
-    public static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "kotlet";
+    private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_NAME = "users";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_EMAIL = "email";
-    public static final String COLUMN_PASSWORD = "password";
-    public static final String COLUMN_FULL_NAME = "full_name";
+    private static final String TABLE_NAME = "users";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_EMAIL = "email";
+    private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_FULL_NAME = "full_name";
 
 
     public DatabaseHelper(@Nullable Context context) {
@@ -27,11 +27,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_EMAIL + " TEXT, " + COLUMN_PASSWORD + " TEXT, " + COLUMN_FULL_NAME + " TEXT)";
+        db.execSQL(query);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
     }
 }
