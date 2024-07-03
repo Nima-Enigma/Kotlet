deb75a7f65bf2ab30d87ad26eb61c04acb4aa2bd
Nima Miri <1381.n.m@gmail.com>
2024-07-03 05:53:13 +0330

    home menu added

--- app/src/main/java/com/jafari/kotletapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/jafari/kotletapp/ui/home/HomeFragment.java
@@ -4,21 +4,29 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.jafari.kotletapp.DatabaseHelper;
+import com.jafari.kotletapp.R;
 import com.jafari.kotletapp.databinding.FragmentHomeBinding;
+
+import java.util.EventListener;
 
 public class HomeFragment extends Fragment {
 
@@ -38,14 +46,22 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
 
 
-        final TextView textView = binding.textHome;
-        final Button btn = binding.addBtn;
+        final Button addBtn = binding.addBtn;
+        GridLayout ingredients = binding.ingredients;
         autoCompleteTextView = binding.autoComplete;
         ArrayAdapter<String> adapter = new ArrayAdapter<>(requireActivity(), android.R.layout.simple_dropdown_item_1line, items);
         autoCompleteTextView.setAdapter(adapter);
         autoCompleteTextView.setThreshold(1);
 
-        btn.setOnClickListener(v -> System.out.println("fuck"));
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String ingredient = autoCompleteTextView.getText().toString();
+                makeCard(ingredients , ingredient);
+                autoCompleteTextView.setText("");
+            }
+        });
         binding.admBtn.setOnClickListener(v -> {
             databaseHelper.addNewIngredient(autoCompleteTextView.getText().toString(), 10);
             items = databaseHelper.allIngredients();
@@ -53,8 +69,35 @@ public class HomeFragment extends Fragment {
             autoCompleteTextView.setAdapter(new_adapter);
             autoCompleteTextView.setThreshold(1);
         });
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+
+    public void makeCard(GridLayout plain , String ingredient){
+        CardView cardView = new CardView(requireActivity());
+        LinearLayout.LayoutParams cardViewParams = new LinearLayout.LayoutParams(
+                450,
+                120  // 40dp in pixels
+        );
+        cardViewParams.setMargins(12, 12, 12, 12);  // 5dp in pixels
+        cardView.setLayoutParams(cardViewParams);
+        cardView.setRadius(90);
+        cardView.setCardBackgroundColor(getResources().getColor(R.color.greenishYellow));
+
+
+        // Create TextView
+        TextView textView = new TextView(requireActivity());
+        LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,  // 290dp in pixels
+                LinearLayout.LayoutParams.MATCH_PARENT
+        );
+        textView.setLayoutParams(textViewParams);
+        textView.setText(ingredient);
+        textView.setTextSize(22);
+        textView.setGravity(Gravity.CENTER);
+
+        // Add LinearLayout to CardView
+        cardView.addView(textView);
+        plain.addView(cardView);
     }
 
     @Override
